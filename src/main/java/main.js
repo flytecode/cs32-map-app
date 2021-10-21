5348dff335a4e4899dda604183bebef9982b1fe0
vivsli <68928756+vivsli@users.noreply.github.com>
2021-10-21 00:13:14 -0400

    javascript set up

---
+++ src/main/java/main.js
@@ -0,0 +1,34 @@
+/* ------ TODO: put global variables here -------- */
+// here's a hint at a potential way to keep track of the current element
+// DO NOT IMPLEMENT THIS BEFORE FIGURING OUT YOUR HTML SPEC AND CORE FUNCTIONALITY
+let CURRENT_ELEMENT = {
+   // a function that updates this.value to newElement and reads the element
+   setAndSpeak: async (newElement) => {},
+   // the current element
+ value: null
+}
+
+
+
+window.onload = () => {
+   /* TODO: initialize Speech API object, inject HTML, get page elements, and initialize event listeners here */
+   let VOICE_SYNTH = window.speechSynthesis
+   /* ... */
+}
+
+
+
+
+/* ------- TODO: write your functions here -------- */
+
+/* ------------------------------------------------- */
+
+
+
+
+// maps element categories to reading handlers (these should return
+// strings)
+const HANDLERS = {}
+
+// maps element tag names to element categories
+const ROLES = {}
