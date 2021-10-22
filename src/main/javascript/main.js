a5264d54884552955b4e0868e2e007c6c76a92a7
GoldenSlayer <nh2jezgoketyfwkv@gmail.com>
2021-10-22 14:53:38 -0400

    refactored

--- src/main/javascript/main.js
+++ src/main/javascript/main.js
@@ -1,34 +1,40 @@
 /* ------ TODO: put global variables here -------- */
+let ALL_ELEMENTS = []
+let PAGE_MAP = {}
 // here's a hint at a potential way to keep track of the current element
 // DO NOT IMPLEMENT THIS BEFORE FIGURING OUT YOUR HTML SPEC AND CORE FUNCTIONALITY
 let CURRENT_ELEMENT = {
    // a function that updates this.value to newElement and reads the element
-   setAndSpeak: async (newElement) => {},
+   set: (newElement) => {},
    // the current element
  value: null
 }
 
-
-
 window.onload = () => {
-   /* TODO: initialize Speech API object, inject HTML, get page elements, and initialize event listeners here */
-   let VOICE_SYNTH = window.speechSynthesis
-   /* ... */
+    /* TODO: initialize Speech API object, inject HTML, get page elements, and initialize event listeners here */
+    mapPage()
+    //let VOICE_SYNTH = window.speechSynthesis
+    //console.log("Loaded Successfully")
 }
 
+const mapPage = () => {
+    if (ALL_ELEMENTS.length === 0){
+        ALL_ELEMENTS = document.body.getElementsByTagName("*")
+    }
 
-
-
-/* ------- TODO: write your functions here -------- */
-
-/* ------------------------------------------------- */
-
-
+    for (let i = 0; i < ALL_ELEMENTS.length; i++) {
+        const currentElement = ALL_ELEMENTS[i]
+        if (!currentElement.id){
+            currentElement.id = i
+        }
+        PAGE_MAP[currentElement.id] = i
+    }
+}
 
 
 // maps element categories to reading handlers (these should return
 // strings)
-const HANDLERS = {}
+// const HANDLERS = {}
 
 // maps element tag names to element categories
-const ROLES = {}
+// const ROLES = {}
