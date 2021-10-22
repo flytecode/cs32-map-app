6bcb125eb1b0ee9e0bbc334d52fe9d2d9199e697
GoldenSlayer <nh2jezgoketyfwkv@gmail.com>
2021-10-22 15:14:59 -0400

    test

--- src/main/javascript/main.js
+++ src/main/javascript/main.js
@@ -4,17 +4,22 @@ let PAGE_MAP = {}
 // here's a hint at a potential way to keep track of the current element
 // DO NOT IMPLEMENT THIS BEFORE FIGURING OUT YOUR HTML SPEC AND CORE FUNCTIONALITY
 let CURRENT_ELEMENT = {
-   // a function that updates this.value to newElement and reads the element
-   set: (newElement) => {},
-   // the current element
- value: null
+    // a function that updates this.value to newElement and reads the element
+    setAndSpeak: async (newElement) => {},
+    // the current element
+    value: null
 }
+
 
 window.onload = () => {
     /* TODO: initialize Speech API object, inject HTML, get page elements, and initialize event listeners here */
     mapPage()
-    //let VOICE_SYNTH = window.speechSynthesis
-    //console.log("Loaded Successfully")
+    let VOICE_SYNTH = window.speechSynthesis
+    for (let i = 0; i < ALL_ELEMENTS.length; i++) {
+        let current = document.getElementById(i.toString()).value
+        let utterThis = new SpeechSynthesisUtterance(current)
+        VOICE_SYNTH.speak(utterThis)
+    }
 }
 
 const mapPage = () => {
@@ -32,9 +37,12 @@ const mapPage = () => {
 }
 
 
-// maps element categories to reading handlers (these should return
-// strings)
-// const HANDLERS = {}
+
+// // just testing writing
+// function title
+
+// maps element categories to reading handlers (these should return strings)
+const HANDLERS = {}
 
 // maps element tag names to element categories
-// const ROLES = {}
+const ROLES = {}
