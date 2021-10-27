0ef71c9503bddc72a01ad6ceb4a17f8b02fc1814
GoldenSlayer <nh2jezgoketyfwkv@gmail.com>
2021-10-27 19:35:44 -0400

    Test
    Added more tags to the test file for a wider coverage of testing

--- src/main/javascript/main.js
+++ src/main/javascript/main.js
@@ -135,6 +135,12 @@ const formHandler = (currentElement) => {
     voiceOver(textToSpeak)
 }
 
+// TODO temporarily the same as the text handler
+const buttonHandler = (currentElement) => {
+    let textToSpeak = currentElement.getAttribute("href")
+    voiceOver(textToSpeak)
+}
+
 
 const voiceOver = (textToSpeak) => {
     let voices = window.speechSynthesis.getVoices()
@@ -154,6 +160,7 @@ const HANDLERS = {
     "table" : tableHandler,
     "multimedia" : multimediaHandler,
     "form" : formHandler,
+    "button" : buttonHandler,
 }
 
 // maps element tag names to element categories
@@ -168,9 +175,10 @@ const ROLES = {
     "MAIN" : "section",
     "NAV" : "section",
     "SECTION" : "section",
+    "LI" : "section",
+
 
     "P" : "text",
-    "TEXT" : "text",
     "H1" : "text",
     "H2" : "text",
     "H3" : "text",
@@ -188,11 +196,10 @@ const ROLES = {
     "CANVAS" : "figures",
     "SVG" : "figures",
 
-    "LI" : "list",
     "UL" : "list",
     "OL" : "list",
 
-    "BUTTON" : "interactive",
+    "BUTTON" : "button",
     "A" : "interactive",
     "INPUT" : "interactive",
 
